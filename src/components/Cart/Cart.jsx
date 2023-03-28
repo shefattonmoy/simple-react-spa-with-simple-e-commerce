632bd131bf1d3ef4a2f48cc4c9365d4524ecc70d
Shefat Tonmoy <shefathossain7@gmail.com>
2023-03-28 17:03:35 +0600

    done

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './Cart.css'
+
+const Cart = (props) => {
+    const {cart} = props;
+
+    let totalPrice = 0;
+    let totalShipping = 0;
+    let quantity = 0;
+
+    for(const product of cart) {
+        product.quantity = product.quantity || 1;
+        totalPrice = totalPrice + product.price * product.quantity;
+        totalShipping = totalShipping + product.shipping;
+        quantity = quantity + product.quantity;
+    }
+
+    const tax = ((totalPrice * 7.5) / 100);
+
+    const grandTotal = totalPrice + totalShipping + tax;
+
+    return (
+        <div className='cart'>
+            <h4>Order Summary</h4>
+            <p>Selected Items: {quantity}</p>
+            <p>Total Price: ${totalPrice}</p>
+            <p>Total Shipping: ${totalShipping}</p>
+            <p>Tax: ${tax.toFixed(2)}</p>
+            <h3>Grand Total: ${grandTotal.toFixed(2)}</h3>
+        </div>
+    );
+};
+
+export default Cart;
---
+++ src/components/Shop/Shop.jsx
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from 'react';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
+import Cart from '../Cart/Cart';
+import Product from '../Product/Product';
+import './Shop.css';
+
+const Shop = () => {
+    const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([])
+
+    useEffect( () => {
+        fetch('products.json')
+        .then(response => response.json())
+        .then(data => setProducts(data))
+    }, [])
+
+    useEffect( () => {
+        const storedCart = getShoppingCart();
+        let savedCart = [];
+
+        // Step 01: Get ID of the addedProduct
+        for(const id in storedCart) {
+            // Step 02: Get the product by using ID
+            const addedProduct = products.find(product => product.id === id)
+            if(addedProduct){
+                // Step 03: Get quantity of the product
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+                // Step 04: Push the addedProduct to the savedCart
+                savedCart.push(addedProduct);
+            }
+        }
+        // Step 05: Set the cart
+        setCart(savedCart);
+    }, [products])
+
+    const handleAddToCart = (product) => {
+        const newCart = [...cart, product];
+        setCart(newCart);
+        addToDb(product.id);
+    }
+
+    return (
+        <div className='shop-container'>
+            <div className="products-container">
+                {
+                    products.map(product => <Product
+                    key={product.id}
+                    product = {product}
+                    handleAddToCart = {handleAddToCart}
+                    ></Product>)
+                }
+            </div>
+            <div className="cart-container">
+                <Cart cart={cart}></Cart>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
